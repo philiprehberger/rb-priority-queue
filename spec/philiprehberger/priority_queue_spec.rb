6587672c89a41a7a760a5ebf0ab53d1f9b6babc2
Reverend Philip <me@philiprehberger.com>
2016-01-16 16:54:51 -0600

    Release v0.1.3

--- spec/philiprehberger/priority_queue_spec.rb
+++ spec/philiprehberger/priority_queue_spec.rb
@@ -235,5 +235,181 @@ RSpec.describe Philiprehberger::PriorityQueue::Queue do
       expect(queue.pop).to eq('second')
       expect(queue.pop).to eq('third')
     end
+
+    it 'maintains FIFO across many items with same priority' do
+      queue = described_class.new
+      items = (1..10).map { |i| "item_#{i}" }
+      items.each { |item| queue.push(item, priority: 5) }
+
+      result = []
+      result << queue.pop until queue.empty?
+      expect(result).to eq(items)
+    end
+  end
+
+  describe 'large queue' do
+    it 'handles 200 items correctly' do
+      queue = described_class.new
+      values = (1..200).to_a.shuffle
+      values.each { |v| queue.push("item_#{v}", priority: v) }
+
+      result = []
+      result << queue.pop until queue.empty?
+      expect(result).to eq((1..200).map { |v| "item_#{v}" })
+    end
+
+    it 'maintains correct size for 100+ items' do
+      queue = described_class.new
+      150.times { |i| queue.push("item_#{i}", priority: i) }
+      expect(queue.size).to eq(150)
+    end
+  end
+
+  describe '#change_priority' do
+    it 'lowers priority so item pops first' do
+      queue = described_class.new
+      queue.push('a', priority: 10)
+      queue.push('b', priority: 20)
+      queue.push('c', priority: 30)
+
+      queue.change_priority('c', 1)
+      expect(queue.pop).to eq('c')
+    end
+
+    it 'raises priority so item pops last' do
+      queue = described_class.new
+      queue.push('a', priority: 1)
+      queue.push('b', priority: 2)
+      queue.push('c', priority: 3)
+
+      queue.change_priority('a', 100)
+      expect(queue.pop).to eq('b')
+      expect(queue.pop).to eq('c')
+      expect(queue.pop).to eq('a')
+    end
+
+    it 'handles changing to the same priority (no-op)' do
+      queue = described_class.new
+      queue.push('a', priority: 5)
+      queue.change_priority('a', 5)
+      expect(queue.pop).to eq('a')
+    end
+  end
+
+  describe '#include?' do
+    it 'returns true for an existing item' do
+      queue = described_class.new
+      queue.push('x', priority: 1)
+      expect(queue.include?('x')).to be true
+    end
+
+    it 'returns false for a missing item' do
+      queue = described_class.new
+      queue.push('x', priority: 1)
+      expect(queue.include?('y')).to be false
+    end
+  end
+
+  describe '#clear and re-add' do
+    it 'allows adding items after clear' do
+      queue = described_class.new
+      queue.push('a', priority: 1)
+      queue.push('b', priority: 2)
+      queue.clear
+
+      queue.push('c', priority: 3)
+      queue.push('d', priority: 1)
+
+      expect(queue.size).to eq(2)
+      expect(queue.pop).to eq('d')
+      expect(queue.pop).to eq('c')
+    end
+  end
+
+  describe '#to_a order verification' do
+    it 'returns items in priority order for min-heap' do
+      queue = described_class.new
+      queue.push('c', priority: 30)
+      queue.push('a', priority: 10)
+      queue.push('b', priority: 20)
+
+      expect(queue.to_a).to eq(%w[a b c])
+    end
+
+    it 'returns items in priority order for max-heap' do
+      queue = described_class.new(mode: :max)
+      queue.push('c', priority: 30)
+      queue.push('a', priority: 10)
+      queue.push('b', priority: 20)
+
+      expect(queue.to_a).to eq(%w[c b a])
+    end
+
+    it 'does not modify the queue' do
+      queue = described_class.new
+      queue.push('a', priority: 1)
+      queue.push('b', priority: 2)
+      queue.to_a
+      expect(queue.size).to eq(2)
+    end
+  end
+
+  describe '#merge with non-empty queues' do
+    it 'interleaves priorities correctly' do
+      q1 = described_class.new
+      q1.push('a', priority: 1)
+      q1.push('c', priority: 5)
+
+      q2 = described_class.new
+      q2.push('b', priority: 3)
+      q2.push('d', priority: 7)
+
+      merged = q1.merge(q2)
+      expect(merged.to_a).to eq(%w[a b c d])
+    end
+  end
+
+  describe 'negative priorities' do
+    it 'handles negative priority values' do
+      queue = described_class.new
+      queue.push('neg', priority: -10)
+      queue.push('pos', priority: 10)
+      queue.push('zero', priority: 0)
+
+      expect(queue.pop).to eq('neg')
+      expect(queue.pop).to eq('zero')
+      expect(queue.pop).to eq('pos')
+    end
+  end
+
+  describe 'float priorities' do
+    it 'orders by float priority values' do
+      queue = described_class.new
+      queue.push('b', priority: 1.5)
+      queue.push('a', priority: 1.1)
+      queue.push('c', priority: 1.9)
+
+      expect(queue.pop).to eq('a')
+      expect(queue.pop).to eq('b')
+      expect(queue.pop).to eq('c')
+    end
+
+    it 'handles very close float priorities' do
+      queue = described_class.new
+      queue.push('a', priority: 1.0000001)
+      queue.push('b', priority: 1.0000002)
+
+      expect(queue.pop).to eq('a')
+      expect(queue.pop).to eq('b')
+    end
+  end
+
+  describe 'pop returns nil on empty' do
+    it 'returns nil after all items popped' do
+      queue = described_class.new
+      queue.push('a', priority: 1)
+      queue.pop
+      expect(queue.pop).to be_nil
+    end
   end
 end
